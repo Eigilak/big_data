23a6da25ce7e09e9d71bf1a87446297d45c62e9e
Eigilak <hello@eigilak.dk>
2021-12-30 10:54:32 +0100

    clean code

--- Øvelser/8 Decision analytic thinking 1/Zenfit _ Dashboard_files/chat-widget.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    $.fn.openChat = function() {
-        var $element = this;
-
-        if ($element.children('.chat-widget-wrap').children('.chat-widget-header').length > 0) {
-            $element.children('.chat-widget-wrap').children('.chat-widget-header').click();
-        }
-    };
-})(jQuery);
